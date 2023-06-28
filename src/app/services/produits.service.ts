1b60afcb0d33127aaa15af6720c79b7b19eefde6
Jean-Yves Rosaye <83571685+Credominute@users.noreply.github.com>
2023-06-28 18:52:56 +0200

    connexion avec le back-end

--- src/app/services/produits.service.ts
+++ src/app/services/produits.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Produit } from '../models/produit.model'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProduitsService {
 
-  constructor() { }
+  constructor(private HttpClient: HttpClient) { }
+
+  getProduits() : Observable<Produit[]> {
+  return this.HttpClient.get<Produit[]>(environment.apiUrl + '/produits')
+  }
 }
